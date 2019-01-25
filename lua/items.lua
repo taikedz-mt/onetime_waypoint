6a53e40a43bfcaee9072a60f635924980fab8bba
Tai Kedzierski <dch.tai@gmail.com>
2019-01-25 00:47:27 +0000

    Add particle effect for added confirmation

--- lua/items.lua
+++ lua/items.lua
@@ -1,3 +1,23 @@
+local function particle_effects(pos, colour)
+    local radius = 2
+    minetest.add_particlespawner({
+            amount = 80,
+            time = 1,
+            minpos = {x=pos.x-radius/2, y=pos.y-radius/2, z=pos.z-radius/2}, 
+            maxpos = {x=pos.x+radius/2, y=pos.y+radius/2, z=pos.z+radius/2}, 
+            minlevel = {x=-0, y=-0, z=-0}, 
+            maxlevel = {x=1, y=1, z=1}, 
+            minacc = {x=-0.5,y=5,z=-0.5}, 
+            maxacc = {x=0.5,y=5,z=0.5}, 
+            minexptime = 0.1, 
+            maxexptime = 1, 
+            minsieze = 3,
+            maxsieze = 4,
+            collisiondetection = false,
+            texture = "tnt_smoke.png^[colorize:"..colour..":80:100"
+    })
+end
+
 minetest.register_craftitem("onetime_waypoint:waypoint", {
     inventory_image = "default_stick.png^[colorize:yellow:140",
     stack_max = 1,
@@ -10,6 +30,7 @@ minetest.register_craftitem("onetime_waypoint:waypoint", {
 
         if not minetest.is_protected(pos, playername) then
             onetime_waypoint:set_waypoint(playername, pos)
+            particle_effects(pos, "purple")
             minetest.chat_send_player(playername, "Waypoint set at "..minetest.pos_to_string(pos))
             itemstack:take_item()
             return itemstack
@@ -23,6 +44,7 @@ minetest.register_craftitem("onetime_waypoint:waypoint", {
         if pos then
             pointer:setpos(pos)
             minetest.chat_send_player(playername, "Waypoint dissolved!")
+            particle_effects(pos, "red")
 
             onetime_waypoint:set_waypoint(playername)
             itemstack:take_item()
